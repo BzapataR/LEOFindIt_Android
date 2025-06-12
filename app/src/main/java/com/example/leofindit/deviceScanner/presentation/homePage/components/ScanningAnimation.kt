41208cdeef99ace4b4b58591cde84b83b402e7d4
Brian Zapata Resendiz <brianzapata.0723@gmail.com>
2025-06-12 16:29:30 -0400

    scanner color fixed

--- app/src/main/java/com/example/leofindit/deviceScanner/presentation/homePage/components/ScanningAnimation.kt
+++ app/src/main/java/com/example/leofindit/deviceScanner/presentation/homePage/components/ScanningAnimation.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -41,8 +42,8 @@ fun ScanningAnimation(
     )
 
     // Updated Colors
-    val primaryColor = Color.Black
-    val secondaryColor = Color(0xFFECC06C)
+    val primaryColor = MaterialTheme.colorScheme.background
+    val secondaryColor = MaterialTheme.colorScheme.primary
 
     // Background modifier
     val backgroundModifier = if (withBackground) {
